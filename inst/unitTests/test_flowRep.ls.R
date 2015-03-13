a89b241ce5aa40648103a0cb144b97d85f322dfb
Josef Spidlen <jspidlen@bccrc.ca>
2015-03-13 12:22:09 -0700

    Starting to add some unit tests.

---
+++ inst/unitTests/test_flowRep.ls.R
@@ -0,0 +1,6 @@
+test_flowRep.ls <- function() {
+  # Not much useful testing as we don't know what 
+  # FlowRepository datasets are (will be) available.
+  # So we will just check that there are at least 50 different datasets
+  checkTrue(length(flowRep.ls()) > 50)
+}
